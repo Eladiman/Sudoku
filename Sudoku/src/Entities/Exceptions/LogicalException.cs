52370017eeef46dba0d8017b5c8cb96cb8db29fb
ram mizrahi <eladmiz2006@gmail.com>
2025-01-18 14:16:07 +0200

    feat/:Exceptions - insert Exceptiond folder into entities

--- Sudoku/src/Entities/Exceptions/LogicalException.cs
+++ Sudoku/src/Entities/Exceptions/LogicalException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sudoku.src.Exceptions
+namespace Sudoku.src.Entities.Exceptions
 {
     internal class LogicalException
     {
