a30160fead8223ed27994a76e11af66868a06d24
ram mizrahi <eladmiz2006@gmail.com>
2025-02-10 19:46:52 +0200

    feat/:CleanCode add tests for simple board 9X9

---
+++ tests/SudokuTests/SmallBoards/HardSmallSudokuTests.cs
@@ -0,0 +1,45 @@
+﻿namespace SudokuTests.SmallBoards
+{
+    public class HardSmallSudokuTests
+    {
+        [Fact]
+        public void HardSmallSudoku1()
+        {
+            string expression = "001000007000890000000000600260030000000500074900000000001040508300000000000000200";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void HardSmallSudoku2()
+        {
+            string expression = "307040000000000918000000004000007000001600000002500000000003800900005000206000000";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void HardSmallSudoku3()
+        {
+            string expression = "000000410900300000300050000048007000000000062010000000600200005070000800000090000";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void HardSmallSudoku4()
+        {
+            string expression = "609000008000701000400000000000006000402000003003000050001050007080009000000000200";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void HardSmallSudoku5()
+        {
+            string expression = "805000002000901000300000000607004002000500000000000600003800000100009000400000070";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+    }
+}
---
+++ tests/SudokuTests/SmallBoards/MediumSmallSudokuTests.cs
@@ -0,0 +1,45 @@
+﻿namespace SudokuTests.SmallBoards
+{
+    public class MediumSmallSudokuTests
+    {
+        [Fact]
+        public void MediumSmallSudoku1()
+        {
+            string expression = "090300020000070500001000000070860040000090200050000000000010006340008000000000000";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void MediumSmallSudoku2()
+        {
+            string expression = "080000063000040200000000000001080350070000090000060000020907000000000003540000000";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void MediumSmallSudoku3()
+        {
+            string expression = "200040500001000003000000000000600008020703090000001000004000506000007000900080000";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void MediumSmallSudoku4()
+        {
+            string expression = "704000002000801000300000000506001002000400000000000900003700000900005000800000060";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+
+        [Fact]
+        public void MediumSmallSudoku5()
+        {
+            string expression = "000040001030600000800000000109005000000000870000200000070000260500094000000000300";
+
+            BaseTests.CheckSudokuSolver(expression);
+        }
+    }
+}
